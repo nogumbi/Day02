44239ac9a46cc034b9a78d911e26d8d758319be2
Nobantu Gumbi <nogumbi@c4r5s8.wethinkcode.co.za>
2020-07-09 14:59:54 +0200

    Prints different combination of two digits function

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,37 @@
+void ft_putchar(char c);
+
+void ft_print_comb2(void)
+{
+	char n1;
+	char n2;
+	char n3;
+	char n4;
+
+	n1 = '0';
+	while( n1 <= '9')
+	{
+		n2 = '0';
+		while(n2 <= '9')
+		{
+			n3 = '0';
+			while(n3 <= '9')
+			{
+				n4 = '1';
+				while(n4 <= '9')
+				{
+					ft_putchar(n1);
+					ft_putchar(n2);
+					ft_putchar(' ');
+					ft_putchar(n3);
+					ft_putchar(n4);
+					ft_putchar(',');
+					ft_putchar(' ');
+					n4++;
+					}
+				n3++;
+			}
+			n2++;
+		}
+	 n1++;
+	}
+}
