a76b590998605000010d64f5edebf1327d02a336
Nobantu Gumbi <nogumbi@c4r5s8.wethinkcode.co.za>
2020-07-09 12:44:47 +0200

    Prints numbers from 012 - 789

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,45 @@
+#include<unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb(void)
+{
+	char n1 = '0';
+	char n2 = '1';
+	char n3 = '2';
+
+	while(n1 <= '7')
+	{
+		n2 = '0';
+		while(n2 <= '8')
+		{
+			n3 = '0';
+			while(n3 <= '9')
+			{
+				if (n1 != n2 && n2 != n3 && n3 != n1)
+				{
+					ft_putchar(n1);
+					ft_putchar(n2);
+					ft_putchar(n3);
+						
+					if( !(n1 == '7' && n2 == '8' && n3 == '9'))
+					{
+						ft_putchar(',');
+						ft_putchar(' ');
+						
+					}
+
+				}
+
+
+				n3++;
+			}
+			n2++;
+		}
+		n1++;
+	}
+
+}
