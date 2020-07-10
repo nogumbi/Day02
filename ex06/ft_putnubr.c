23c2e933fe9670e686e90a06da547202bdfe76fc
Nobantu Gumbi <nogumbi@c4r18s1.wethinkcode.co.za>
2020-07-10 10:07:36 +0200

    pt_ptunbr function

---
+++ ex06/ft_putnubr.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+		if(nb < 10)
+		{
+			ft_putchar(nb + '0');
+		}
+		else
+		{
+			ft_putnbr(nb/10);
+			ft_putchar(nb%10 + '0');
+		}
+}
+
